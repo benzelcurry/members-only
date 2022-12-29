13212df06241b5e8a8b2667366e82af37d21790c
Ben <benawhiting@gmail.com>
2022-12-29 15:26:37 -0800

    Create model for messages

---
+++ models/message.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const { DateTime } = require('luxon');
+
+const MessageSchema = new Schema({
+  title: { type: String, required: true, maxLength: 100 },
+  content: { type: String, required: true, maxLength: 1000 },
+  date: { type: Date },
+  author: { type: Schema.Types.ObjectId, ref: 'Member', required: true },
+});
+
+MessageSchema.virtual('url').get(function() {
+  return `/members/${this._id}`;
+});
+
+MessageSchema.virtual('date_formatted').get(function() {
+  return DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATE_MED);
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
