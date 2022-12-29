99baeff92b85ef4d059a83afe24ee9f5f2d3093c
Ben <benawhiting@gmail.com>
2022-12-29 15:13:44 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+require('dotenv').config();
+
+const mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongo connection error'));
+
+const app = express();
+app.set('views', __dirname);
+app.set('view engine', 'ejs');
+
+app.use(session({
+  secret: 'cats',
+  resave: false,
+  saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => res.send('<h1>Members Only</h1>'));
+
+app.listen(3000, () => console.log('App listening on Port 3000!'));
